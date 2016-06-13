c8d4b1d69e51c39fd2a25a5b6c6890c72cd247eb
lengxf <lengxf@qq.com>
2016-06-14 00:14:47 +0800

    init

---
+++ setting/02_SetX_CurrentUser_EviromentPath - work.bat
@@ -0,0 +1,33 @@
+
+
+@echo 设置path
+
+
+@IF not EXIST %systemroot%\system32\setx.exe (
+
+@ECHO 未能找到%systemroot%\system32\setx.exe，请下载setx.exe程序
+pause
+exit
+
+) 
+
+setx MYBIN %USERPROFILE%\bin
+
+setx JAVA_HOME "C:\Program Files\Java\jdk1.8.0_91"
+
+::setx SCALA_HOME "C:\Program Files (x86)\scala"
+
+::setx SBT_HOME "C:\Program Files (x86)\sbt"
+
+
+setx ANDROID_HOME "D:\Android\Sdk"
+
+setx GRADLE_HOME  "D:\Android\gradle\gradle-2.13"
+
+
+
+::setx PATH "%%PATH%%;%%JAVA_SDK_HOME%%\bin;%%ANDROID_SDK_HOME%%\tools;%%ANDROID_SDK_HOME%%\platform-tools;%%GRADLE_HOME%%\bin;%%MYBIN%%;%%SCALA_HOME%%\bin;%%SBT_HOME%%\bin"
+setx PATH "%%PATH%%;%%JAVA_HOME%%\bin;%%ANDROID_SDK_HOME%%\tools;%%ANDROID_HOME%%\platform-tools;%%GRADLE_HOME%%\bin;%%MYBIN%%"
+pause
+
+
