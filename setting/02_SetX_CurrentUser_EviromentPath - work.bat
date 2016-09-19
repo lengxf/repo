63b3b7d9b65eba3d65d08088b87a7adb6bf27bbd
Lion leng <lion.leng@fbi.china.com>
2016-09-19 18:17:48 +0800

    add setting

--- setting/02_SetX_CurrentUser_EviromentPath - work.bat
+++ setting/02_SetX_CurrentUser_EviromentPath - work.bat
@@ -11,23 +11,22 @@ exit
 
 ) 
 
-setx MYBIN %USERPROFILE%\bin
 
-setx JAVA_HOME "C:\Program Files\Java\jdk1.8.0_91"
+setx JAVA_HOME "C:\Program Files\Java\jdk1.8.0_91" 
+set tempPath=%tempPath%;%%JAVA_HOME%%
 
-::setx SCALA_HOME "C:\Program Files (x86)\scala"
+IF EXIST "%USERPROFILE%\bin"                   			   (setx MYBIN %USERPROFILE%\bin && set tempPath=%tempPath%;%%MYBIN%%)
+IF EXIST "C:\Program Files\Git\bin" 			           (setx  GIT "C:\Program Files\Git\bin" && set tempPath=%tempPath%;%%GIT%%)
+IF EXIST "C:\Program Files (x86)\scala\bin"    			    (setx SCALA_HOME "C:\Program Files (x86)\scala" && set tempPath=%tempPath%;%%SCALA_HOME%%)
+IF EXIST "C:\Program Files (x86)\sbt\bin"                  (setx SBT_HOME "C:\Program Files (x86)\sbt" && set tempPath=%tempPath%;%%SBT_HOME%%)
+IF EXIST "D:\Android\Sdk"                                  (setx ANDROID_HOME "D:\Android\Sdk" && set tempPath=%tempPath%;%%ANDROID_HOME%%\platform-tools)
+IF EXIST "D:\Android\gradle\gradle-2.14.1\bin"                (setx GRADLE  "D:\Android\gradle\gradle-2.14.1\bin" && set tempPath=%tempPath%;%%GRADLE%%)
+;IF EXIST "D:\Program Files\graphviz-2.38\release\bin"       (setx GRAPHVIZ_DOT "D:\Program Files\graphviz-2.38\release\bin" && set tempPath=%tempPath%;%%GRAPHVIZ_DOT%%)
+IF EXIST "D:\Android\Android Studio\bin" 					(setx android_studio "D:\Android\Android Studio\bin" && set tempPath=%tempPath%;%%android_studio%%)
+IF EXIST "C:\Program Files (x86)\pgAdmin III\1.22" 			(setx  pgAdmin "C:\Program Files (x86)\pgAdmin III\1.22" && set tempPath=%tempPath%;%%pgAdmin%%)
+IF EXIST "C:\Python\Python35" 			(setx  Python35 "C:\Python\Python35" && set tempPath=%tempPath%;%%Python35%%)
 
-::setx SBT_HOME "C:\Program Files (x86)\sbt"
-
-
-setx ANDROID_HOME "D:\Android\Sdk"
-
-setx GRADLE_HOME  "D:\Android\gradle\gradle-2.13"
-
-
-
-::setx PATH "%%PATH%%;%%JAVA_SDK_HOME%%\bin;%%ANDROID_SDK_HOME%%\tools;%%ANDROID_SDK_HOME%%\platform-tools;%%GRADLE_HOME%%\bin;%%MYBIN%%;%%SCALA_HOME%%\bin;%%SBT_HOME%%\bin"
-setx PATH "%%PATH%%;%%JAVA_HOME%%\bin;%%ANDROID_SDK_HOME%%\tools;%%ANDROID_HOME%%\platform-tools;%%GRADLE_HOME%%\bin;%%MYBIN%%"
+setx PATH "%%PATH%%;%tempPath%"
 pause
 
 
